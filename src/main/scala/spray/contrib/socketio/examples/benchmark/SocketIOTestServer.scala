2e499a1af637afcec18e6ec377ec61d1c1f61fda
Xingrun CHEN <chenxingrun@wandoujia.com>
2014-03-07 16:33:10 +0800

    load host/port config for testserver

--- src/main/scala/spray/contrib/socketio/examples/benchmark/SocketIOTestServer.scala
+++ src/main/scala/spray/contrib/socketio/examples/benchmark/SocketIOTestServer.scala
@@ -12,6 +12,7 @@ import spray.contrib.socketio
 import spray.contrib.socketio.Namespace
 import spray.contrib.socketio.Namespace.OnEvent
 import spray.contrib.socketio.SocketIOServerConnection
+import com.typesafe.config.ConfigFactory
 
 object SocketIOTestServer extends App {
 
@@ -49,6 +50,9 @@ object SocketIOTestServer extends App {
   import system.dispatcher
 
   val server = system.actorOf(Props(classOf[SocketIOServer], namespaces), "socketio")
+  val config = ConfigFactory.load().getConfig("spray.socketio.benchmark")
+  val host = config.getString("host")
+  val port = config.getInt("port")
 
-  IO(UHttp) ! Http.Bind(server, "localhost", 8080)
+  IO(UHttp) ! Http.Bind(server, host, port)
 }

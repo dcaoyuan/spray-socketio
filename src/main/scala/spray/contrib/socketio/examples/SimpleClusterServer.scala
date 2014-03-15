907a681d872dca9b611efe2fa0cf1e110a0d6ec8
Xingrun CHEN <chenxingrun@wandoujia.com>
2014-03-15 17:15:47 +0800

    Add SimpleClusterServer

---
+++ src/main/scala/spray/contrib/socketio/examples/SimpleClusterServer.scala
@@ -0,0 +1,113 @@
+package spray.contrib.socketio.examples
+
+import com.typesafe.config.{Config, ConfigFactory}
+import akka.actor.{Props, ActorSystem}
+import spray.contrib.socketio.{ClusterConnectionActive, ConnectionActive}
+import akka.contrib.pattern.{DistributedPubSubExtension, ClusterSharding}
+import akka.cluster.Cluster
+import akka.persistence.Persistence
+import spray.contrib.socketio.examples.benchmark.SocketIOTestServer.SocketIOServer
+import akka.io.IO
+import spray.can.server.UHttp
+import spray.can.Http
+import rx.lang.scala.Observer
+import spray.contrib.socketio.namespace.Namespace.OnEvent
+import spray.contrib.socketio.packet.MessagePacket
+import spray.contrib.socketio.namespace.{Namespace, ClusterNamespace}
+import akka.persistence.journal.leveldb.{SharedLeveldbJournal, SharedLeveldbStore}
+
+object SimpleClusterServer extends App with MySslConfiguration {
+  val usage = """
+    Usage: SimpleClusterServer [transport|connectionActive|business] -Dakka.cluster.seed-nodes.0=akka.tcp://ClusterSystem@host1:port -Dakka.remote.netty.tcp.hostname=host1 -Dakka.remote.netty.tcp.port=port
+              """
+
+  def exitWithUsage = {
+    println(usage)
+    sys.exit(1)
+  }
+
+  def startCluster(config: Config): ActorSystem = {
+    val system = ActorSystem("ClusterSystem", config)
+    Cluster(system)
+    ClusterSharding(system).start(
+      typeName = ConnectionActive.shardName,
+      entryProps = Some(Props(classOf[ClusterConnectionActive])),
+      idExtractor = ClusterConnectionActive.idExtractor,
+      shardResolver = ClusterConnectionActive.shardResolver)
+    DistributedPubSubExtension(system).mediator
+    system
+  }
+
+  if (args.length == 0) {
+    exitWithUsage
+  }
+  val arglist = args.toList
+
+  import ConfigFactory._
+
+  val commonSettings = load(ConfigFactory.parseString(
+    """
+      |akka.loglevel = INFO
+      |akka.actor.provider = "akka.cluster.ClusterActorRefProvider"
+      |akka.persistence.journal.plugin = "akka.persistence.journal.leveldb-shared"
+      |akka.persistence.journal.leveldb-shared.store {
+      |  native = off
+      |  dir = "target/test-shared-journal"
+      |}
+      |akka.persistence.snapshot-store.local.dir = "target/test-snapshots"
+      |akka.contrib.cluster.sharding.role = "connectionActive"
+      |transport.hostname = "0.0.0.0"
+      |transport.port = 8080
+    """.stripMargin))
+
+  implicit var system: ActorSystem = _
+
+  arglist match {
+    case "transport" :: tail =>
+      val config = parseString("akka.cluster.roles =[\"transport\"]").withFallback(commonSettings)
+      system = startCluster(config)
+
+      implicit val resolver = ClusterSharding(system).shardRegion(ConnectionActive.shardName)
+      val server = system.actorOf(Props(classOf[SocketIOServer], resolver), name = "socketio-server")
+      val host = config.getString("transport.hostname")
+      val port = config.getInt("transport.port")
+      IO(UHttp) ! Http.Bind(server, host, port)
+
+    case "connectionActive" :: tail =>
+      val config = parseString("akka.cluster.roles =[\"connectionActive\"]").withFallback(commonSettings)
+      system = startCluster(config)
+      Persistence(system)
+      val sharedStore = system.actorOf(Props[SharedLeveldbStore], "store")
+      SharedLeveldbJournal.setStore(sharedStore, system)
+
+    case "business" :: tail =>
+      val config = parseString("akka.cluster.roles =[\"business\"]").withFallback(commonSettings)
+      system = startCluster(config)
+      implicit val resolver = ClusterSharding(system).shardRegion(ConnectionActive.shardName)
+
+      val observer = Observer[OnEvent](
+        (next: OnEvent) => {
+          next match {
+            case OnEvent("chat", args, context) =>
+              spray.json.JsonParser(args) // test spray-json too.
+              next.replyEvent("chat", args)(resolver)
+            case OnEvent("broadcast", args, context) =>
+              next.broadcast("", MessagePacket(0, false, "", args))(resolver)
+            case _ =>
+              println("observed: " + next.name + ", " + next.args)
+          }
+        })
+
+      val tmp = system
+      import tmp.dispatcher
+      ClusterNamespace(system)("") map Namespace.subscribe(observer)
+
+    case _ =>
+      exitWithUsage
+  }
+
+  readLine("Hit ENTER to exit ...\n")
+  system.shutdown()
+  system.awaitTermination()
+
+}

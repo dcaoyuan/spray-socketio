f77d7359878de195e4c066dbd2a30502672ec8ed
Caoyuan Deng <dcaoyuan@gmail.com>
2014-10-19 00:15:43 +0800

    Fixed Channel for "onNext is not allowed when the stream has not requested elements, totalDemand was 0'. The socketio-benchmark now works as expected.

--- spray-socketio/src/main/scala/spray/contrib/socketio/namespace/Channel.scala
+++ spray-socketio/src/main/scala/spray/contrib/socketio/namespace/Channel.scala
@@ -2,6 +2,8 @@ package spray.contrib.socketio.namespace
 
 import akka.actor.Props
 import akka.stream.actor.ActorPublisher
+import akka.stream.actor.ActorPublisherMessage
+import scala.annotation.tailrec
 import spray.contrib.socketio.ConnectionSession.OnPacket
 import spray.contrib.socketio.namespace.Namespace.OnConnect
 import spray.contrib.socketio.namespace.Namespace.OnData
@@ -14,18 +16,50 @@ import spray.contrib.socketio.packet.DisconnectPacket
 import spray.contrib.socketio.packet.EventPacket
 import spray.contrib.socketio.packet.JsonPacket
 import spray.contrib.socketio.packet.MessagePacket
+import spray.contrib.socketio.packet.Packet
 
 object Channel {
   def props() = Props(classOf[Channel])
 }
 
 class Channel extends ActorPublisher[OnData] {
+  private var buf = Vector.empty[OnPacket[_]]
+
   def receive = {
-    case OnPacket(packet: ConnectPacket, connContext)    => onNext(OnConnect(packet.args, connContext)(packet))
-    case OnPacket(packet: DisconnectPacket, connContext) => onNext(OnDisconnect(connContext)(packet))
-    case OnPacket(packet: MessagePacket, connContext)    => onNext(OnMessage(packet.data, connContext)(packet))
-    case OnPacket(packet: JsonPacket, connContext)       => onNext(OnJson(packet.json, connContext)(packet))
-    case OnPacket(packet: EventPacket, connContext)      => onNext(OnEvent(packet.name, packet.args, connContext)(packet))
+    case x: OnPacket[_] =>
+      if (buf.isEmpty && totalDemand > 0) {
+        onPacket(x)
+      } else {
+        buf :+= x
+        deliverBuf()
+      }
+    case ActorPublisherMessage.Request(_) =>
+      deliverBuf()
+    case ActorPublisherMessage.Cancel =>
+      context.stop(self)
+  }
+
+  @tailrec
+  final def deliverBuf(): Unit =
+    if (totalDemand > 0) {
+      if (totalDemand <= Int.MaxValue) {
+        val (use, keep) = buf.splitAt(totalDemand.toInt)
+        buf = keep
+        use foreach onPacket
+      } else {
+        val (use, keep) = buf.splitAt(Int.MaxValue)
+        buf = keep
+        use foreach onPacket
+        deliverBuf()
+      }
+    }
+
+  final def onPacket(x: OnPacket[_]) = x.packet match {
+    case packet: ConnectPacket    => onNext(OnConnect(packet.args, x.connContext)(packet))
+    case packet: DisconnectPacket => onNext(OnDisconnect(x.connContext)(packet))
+    case packet: MessagePacket    => onNext(OnMessage(packet.data, x.connContext)(packet))
+    case packet: JsonPacket       => onNext(OnJson(packet.json, x.connContext)(packet))
+    case packet: EventPacket      => onNext(OnEvent(packet.name, packet.args, x.connContext)(packet))
   }
 }
 

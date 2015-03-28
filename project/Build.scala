37d4e653d5a031509b8e4cac48f5bba1c6a718cf
Caoyuan Deng <dcaoyuan@gmail.com>
2015-03-28 15:19:12 +0800

    Upgraded to spray 1.3.3

--- project/Build.scala
+++ project/Build.scala
@@ -122,7 +122,7 @@ object Build extends sbt.Build {
 }
 
 object Dependencies {
-  val SPRAY_VERSION = "1.3.2"
+  val SPRAY_VERSION = "1.3.3"
   val AKKA_VERSION = "2.3.9"
 
   val spray_websocket = "com.wandoulabs.akka" %% "spray-websocket" % "0.1.5-SNAPSHOT"
@@ -130,13 +130,14 @@ object Dependencies {
   val spray_json = "io.spray" %% "spray-json" % "1.3.1"
   val akka_actor = "com.typesafe.akka" %% "akka-actor" % AKKA_VERSION
   val akka_contrib = "com.typesafe.akka" %% "akka-contrib" % AKKA_VERSION
-  val akka_stream = "com.typesafe.akka" %% "akka-stream-experimental" % "1.0-M4"
+  val akka_stream = "com.typesafe.akka" %% "akka-stream-experimental" % "1.0-M5"
   val parboiled = "org.parboiled" %% "parboiled" % "2.0.1"
   val akka_testkit = "com.typesafe.akka" %% "akka-testkit" % AKKA_VERSION % Test
   val akka_multinode_testkit = "com.typesafe.akka" %% "akka-multi-node-testkit" % AKKA_VERSION % "test"
-  val scalatest = "org.scalatest" %% "scalatest" % "2.1.3" % Test
+  val scalatest = "org.scalatest" %% "scalatest" % "2.2.4" % Test
+  val scalaspecs = "org.specs2" %% "specs2-core" % "2.3.13" % Test
   val apache_math = "org.apache.commons" % "commons-math3" % "3.2" // % "test"
-  val caliper = "com.google.caliper" % "caliper" % "0.5-rc1" % "test"
+  val caliper = "com.google.caliper" % "caliper" % "0.5-rc1" % Test
   val akka_persistence_cassandra = "com.github.krasserm" %% "akka-persistence-cassandra" % "0.3.6" % Runtime
 
   val kafka = "org.apache.kafka" %% "kafka" % "0.8.2.1" excludeAll (
@@ -149,7 +150,7 @@ object Dependencies {
   val logback = "ch.qos.logback" % "logback-classic" % "1.0.13"
   val akka_slf4j = "com.typesafe.akka" %% "akka-slf4j" % AKKA_VERSION
 
-  val all = Seq(spray_websocket, spray_can, spray_json, akka_actor, akka_contrib, akka_stream, parboiled, akka_testkit, akka_multinode_testkit, scalatest, apache_math, caliper, logback, akka_slf4j, kafka)
+  val all = Seq(spray_websocket, spray_can, spray_json, akka_actor, akka_contrib, akka_stream, parboiled, akka_testkit, akka_multinode_testkit, scalatest, scalaspecs, apache_math, caliper, logback, akka_slf4j, kafka)
 }
 
 object Formatting {
